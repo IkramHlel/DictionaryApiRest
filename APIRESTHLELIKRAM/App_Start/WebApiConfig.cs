c48ef568f7beb03daf85b09d778d3a7ded2b9b29
IkramHlel <44326234+IkramHlel@users.noreply.github.com>
2020-12-24 02:30:25 +0100

    Add files via upload

---
+++ APIRESTHLELIKRAM/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace APIRESTHLELIKRAM
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
